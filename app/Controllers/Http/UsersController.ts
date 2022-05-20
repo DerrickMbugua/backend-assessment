86774af560370f6d72a870ea1535746172631eae
Derrick Mwema <derrickmbugua50@gmail.com>
2022-05-20 09:55:11 +0300

    feat: error handling

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -3,38 +3,58 @@ import User from 'App/Models/User'
 
 export default class UsersController {
   // Show all Users
-  public async index ({ }: HttpContextContract) {
-    return User.all()
+  public async index ({ response}: HttpContextContract) {
+    try {
+      return User.all()
+    } catch (error) {
+      return response.json(error)
+    }
   }
 
   // Create a new user and save in the db
   public async store ({ request, response }: HttpContextContract) {
-    const body = request.body()
-    const user = await User.create(body)
-    response.status(201)
-    return user
+    try {
+      const body = request.body()
+      const user = await User.create(body)
+      response.status(201)
+      return user
+    } catch (error) {
+      return response.json(error)
+    }
   }
 
   // Show a specific user
-  public async show ({ params }: HttpContextContract) {
-    const user = await User.findOrFail(params.id)
-    return user
+  public async show ({ params, response }: HttpContextContract) {
+    try{
+      const user = await User.findOrFail(params.id)
+      return user
+    } catch (error){
+      return response.json(error)
+    }
   }
 
   // Update a user
-  public async update ({ params, request }: HttpContextContract) {
-    const body = request.body()
-    const user = await User.findOrFail(params.id)
-    user.name = body.name
-    user.email = body.email
-    user.department = body.department
-    return user.save()
+  public async update ({ params, request, response }: HttpContextContract) {
+    try{
+      const body = request.body()
+      const user = await User.findOrFail(params.id)
+      user.name = body.name
+      user.email = body.email
+      user.department = body.department
+      return user.save()
+    }catch(error){
+      return response.json(error)
+    }
   }
 
   // Delete a user
-  public async destroy ({ params }: HttpContextContract) {
-    const user = await User.findOrFail(params.id)
-    user.delete()
-    return 'User deleted successfully'
+  public async destroy ({ params, response }: HttpContextContract) {
+    try {
+      const user = await User.findOrFail(params.id)
+      user.delete()
+      return 'User deleted successfully'
+    } catch (error) {
+      return response.json(error)
+    }
   }
 }
